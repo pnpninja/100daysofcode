207ef5637bdac13e3f41e08110bedf8a4dcac147
Prateek Narendra <prateeknar@gmail.com>
2020-09-18 15:45:02 -0400

    Bugfix for Mathworks

--- src/main/java/edu/stonybrook/pnarendra/solutions/Mathworks_MathCodingPuzzle.java
+++ src/main/java/edu/stonybrook/pnarendra/solutions/Mathworks_MathCodingPuzzle.java
@@ -3,7 +3,7 @@ package edu.stonybrook.pnarendra.solutions;
 public class Mathworks_MathCodingPuzzle {
 	
 	public static int minNum(int A, int K, int P) {
-		if(K <= A && P > 0) {
+		if((K <= A && P > 0) || (A == K)) {
 			return -1;
 		}
 		return P/(K-A) + 1;
